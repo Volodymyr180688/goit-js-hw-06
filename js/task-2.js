45699f6ad5984a079e2a7405ea1d53c492bbda03
Volodymyr180688 <shurik180688@gmail.com>
2024-02-08 19:18:04 +0200

    Homework #6

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,24 @@
 'use strict';
+class Storage {
+    #items;
+    constructor(items) {
+        this.#items = items;
+    }
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        if (this.#items.includes(itemToRemove)) {
+        this.#items = this.#items.filter(item => item !== itemToRemove);
+    }
+    }
+}
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems());
+storage.addItem("Droid");
+console.log(storage.getItems());
+storage.removeItem("Prolonger");
+console.log(storage.getItems());
